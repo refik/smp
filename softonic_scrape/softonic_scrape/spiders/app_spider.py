b09049d0334e55c40ffc5385dc69f394b9a2582f
Refik Turkeli <refik.rfk@gmail.com>
2017-10-03 15:51:57 +0300

    added parsing app logo

--- softonic_scrape/softonic_scrape/spiders/app_spider.py
+++ softonic_scrape/softonic_scrape/spiders/app_spider.py
@@ -1,21 +1,31 @@
 import scrapy
 import re
 
+clean_article_regex = re.compile(r'\n|\t| class=\"app-softonic-review__content \"')
+
 class AppSpider(scrapy.Spider):
     name = 'app'
 
     start_urls = [
-        'https://amazon-compras.softonic.com/android',
-        'https://moboplay.en.softonic.com/'
+        'https://skim.en.softonic.com/mac',
+        'https://moboplay.en.softonic.com/',
+        'https://the-unarchiver.en.softonic.com/mac'
     ]
 
     clean_regex = re.compile(r'\n|\t| class=\"app-softonic-review__content \"')
 
     def parse(self, response):
         review_node = response.xpath("//article[@class='app-softonic-review__content ']")
-        review = self.clean_regex.sub('', review_node.extract_first())
+        review = clean_article_regex.sub('', review_node.extract_first())
+
+        logo_url = response.\
+            xpath('//header/div[contains(@class, "media-app__image")]/img/@src').\
+            extract_first()
+
         app_name = response.xpath("//h1[@class='media-app__title']/text()").extract_first()
+
         yield {
             'app_name': app_name,
+            'logo_url': logo_url,
             'article': review
         }
