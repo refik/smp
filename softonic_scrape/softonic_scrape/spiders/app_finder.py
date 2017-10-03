dec6011bf186c7d860be4c7bc0c948af464f6977
Refik Turkeli <refik.rfk@gmail.com>
2017-10-03 19:40:33 +0300

    change to double quote

--- softonic_scrape/softonic_scrape/spiders/app_finder.py
+++ softonic_scrape/softonic_scrape/spiders/app_finder.py
@@ -1,27 +1,27 @@
 import scrapy
 import re
 
-clean_article_regex = re.compile(r'\n|\t| class=\"app-softonic-review__content \"')
+clean_article_regex = re.compile(r"\n|\t| class=\'app-softonic-review__content '")
 
 class FindAppSpider(scrapy.Spider):
-    name = 'app_finder'
+    name = "app_finder"
 
-    start_urls = ['https://en.softonic.com/windows']
+    start_urls = ["https://en.softonic.com/windows"]
 
     def parse(self, response):
-        category_urls = response.xpath('//div[@class="standar_nav"]//a/@href').extract()
+        category_urls = response.xpath("//div[@class='standar_nav']//a/@href").extract()
 
         for url in category_urls:
             yield scrapy.Request(url, self.parse_sub_category)
 
     def parse_sub_category(self, response):
-        sub_category_urls = response.xpath('//div[@id="categories_nav"]//a/@href').extract()
+        sub_category_urls = response.xpath("//div[@id='categories_nav']//a/@href").extract()
 
         for url in sub_category_urls:
             yield scrapy.Request(url, self.parse_app_list)
 
     def parse_app_list(self, response):
-        app_urls = response.xpath('//div[contains(@class, "content")]/ul/li/a/@href').extract()
+        app_urls = response.xpath("//div[contains(@class, 'content')]/ul/li/a/@href").extract()
 
         for url in app_urls:
             yield scrapy.Request(url, self.parse_app)
@@ -30,18 +30,18 @@ class FindAppSpider(scrapy.Spider):
         review = response.xpath("//article[@class='app-softonic-review__content ']").extract_first()
 
         if review:
-            review = clean_article_regex.sub('', review)
+            review = clean_article_regex.sub("", review)
 
         logo_url = response.\
-            xpath('//header/div[contains(@class, "media-app__image")]/img/@src').\
+            xpath("//header/div[contains(@class, 'media-app__image')]/img/@src").\
             extract_first()
 
         app_name = response.xpath("//h1[@class='media-app__title']/text()").extract_first().strip()
         categories = response.xpath("//li[@class='list-tags__item']/a/@title").extract()
 
         yield {
-            'app_name': app_name,
-            'logo_url': logo_url,
-            'article': review,
-            'categories': categories
+            "app_name": app_name,
+            "logo_url": logo_url,
+            "article": review,
+            "categories": categories
         }
