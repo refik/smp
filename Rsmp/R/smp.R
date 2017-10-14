3ad1e89d3307eb63c2bd8009976b324afb268866
Refik Turkeli <refik.rfk@gmail.com>
2017-10-14 23:47:39 +0300

    app list exploration - close #6

--- Rsmp/R/smp.R
+++ Rsmp/R/smp.R
@@ -1,6 +1,5 @@
-#' @importFrom dplyr %>%
-#' @importFrom dplyr if_else
-#' @importFrom rlang .data quo
+#' @importFrom dplyr %>% if_else
+#' @importFrom rlang .data quo enquo quo_name
 #' @importFrom assertthat assert_that
 #' @importFrom glue glue
 NULL
@@ -23,20 +22,28 @@ db_con <- memoise::memoise(function() {
 #'
 #' @export
 json_populate <- function(.data, json_field, keys_to_populate,
-                          json_key_suffix = "", unselect_json_field = TRUE) {
+                          json_key_suffix = "",
+                          select_keys_to_populate = TRUE) {
   quosures <- keys_to_populate %>%
     purrr::map_chr(~glue("{json_field} ->> '{.}'")) %>%
     purrr::map(~quo(sql(!!.))) %>%
     purrr::set_names(paste0(json_key_suffix, keys_to_populate))
 
-  dplyr::mutate(.data, !!!quosures) %>%
-    dplyr::select(-.data[[json_field]])
+  result <- dplyr::mutate(.data, !!!quosures)
+
+  if (select_keys_to_populate) {
+    result <- dplyr::select(result, !!keys_to_populate)
+  }
+
+  result
 }
 
 #' Scrapy Scrape Table
 #'
 #' @export
-data_scrapy_scrape <- function(keys_to_populate, callback_function = NULL) {
+data_scrape <- function(keys_to_populate, callback_function = NULL,
+                        use_last_spider_results = TRUE,
+                        select_populated = TRUE) {
   scraped_item <- dplyr::tbl(db_con(), "mvp_scrapeditem")
 
   if (!is.null(callback_function)) {
@@ -44,5 +51,59 @@ data_scrapy_scrape <- function(keys_to_populate, callback_function = NULL) {
                                   callback_function == !!callback_function)
   }
 
-  json_populate(scraped_item, "item", keys_to_populate)
+  if (use_last_spider_results) {
+    scraped_item <- filter_last_spider_run(scraped_item)
+  }
+
+  populated <- json_populate(scraped_item, "item", keys_to_populate)
+
+  if (select_populated) {
+    dplyr::select(populated, !!keys_to_populate)
+  }
+}
+
+#' Softonic Apps
+#'
+#' @export
+data_softonic_app <- function(use_last_spider_results = TRUE) {
+  scraped_item <- dplyr::tbl(db_con(), "mvp_scrapeditem")
+
+  if (use_last_spider_results) {
+    scraped_item <- filter_last_spider_run(scraped_item)
+  }
+
+  apps <- scraped_item %>%
+    dplyr::filter(callback_function == "parse_app_list") %>%
+    json_populate(
+      "item",
+      c("url", "cons", "name", "pros", "type", "score", "votes", "license",
+        "summary", "version", "category", "language", "logo_url", "platform",
+        "page_order", "page_number", "sub_category", "download_count"))
+
+  apps %>%
+    dplyr::mutate(score = if_else(score == "-", NA, score)) %>%
+    dplyr::mutate(download_count_prefix = sql("substring(download_count, '[^0-9]+')")) %>%
+    dplyr::mutate(download_count = as.integer(sql("substring(download_count, '[0-9]+')"))) %>%
+    dplyr::mutate(download_count = if_else(download_count_prefix == "M", download_count * (10 ** 6),
+                                           if_else(download_count_prefix == "K", download_count * (10 ** 3),
+                                                   download_count))) %>%
+    dplyr::select(-.data$download_count_prefix) %>%
+    dplyr::mutate(votes = as.integer(votes))
+}
+
+filter_last_spider_run <- function(.data) {
+  last_spider_run <- dplyr::tbl(db_con(), "mvp_scrapeditem") %>%
+    dplyr::summarise(spider_load_time = max(spider_load_time))
+
+  dplyr::semi_join(.data, last_spider_run, by = "spider_load_time")
+}
+
+#' Arrange Descending Nulls Last
+#'
+#' PostgreSQL helper function
+#'
+#' @export
+desc_nulls_last <- function(.data, column) {
+  sql_stmt <- glue("{column} DESC NULLS LAST")
+  dplyr::arrange(.data, sql(sql_stmt))
 }
